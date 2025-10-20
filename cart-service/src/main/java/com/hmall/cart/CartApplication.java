465fd3c93bcdf8592df8ead84046c967854f447b
MeiDaiAa <114286638+MeiDaiAa@users.noreply.github.com>
2025-10-20 20:18:49 +0800

    feat(user): 拆分用户模块

--- cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -1,12 +1,13 @@
 package com.hmall.cart;
 
+import com.hmall.common.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 // 启用OpenFeign
-@EnableFeignClients(basePackages = "com.hmall.api.client")
+@EnableFeignClients(basePackages = "com.hmall.api.client", defaultConfiguration = DefaultFeignConfig.class)
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
 public class CartApplication {
