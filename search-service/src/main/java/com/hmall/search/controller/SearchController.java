0773f94e4b65f9b19fa6aae558b3af265313a99b
MeiDaiAa <114286638+MeiDaiAa@users.noreply.github.com>
2025-10-30 12:38:44 +0800

    feat(search): 过滤条件聚合返回符合搜索选项的分类与品牌

--- search-service/src/main/java/com/hmall/search/controller/SearchController.java
+++ search-service/src/main/java/com/hmall/search/controller/SearchController.java
@@ -6,14 +6,16 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmall.common.domain.PageDTO;
 import com.hmall.search.domain.dto.ItemDTO;
 import com.hmall.search.domain.po.Item;
+import com.hmall.search.domain.po.ItemDoc;
 import com.hmall.search.domain.query.ItemPageQuery;
 import com.hmall.search.service.ISearchService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @Api(tags = "搜索相关接口")
 @RestController
@@ -26,15 +28,24 @@ public class SearchController {
     @ApiOperation("搜索商品")
     @GetMapping("/list")
     public PageDTO<ItemDTO> search(ItemPageQuery query) {
-        // 分页查询
-        Page<Item> result = searchService.lambdaQuery()
-                .like(StrUtil.isNotBlank(query.getKey()), Item::getName, query.getKey())
-                .eq(StrUtil.isNotBlank(query.getBrand()), Item::getBrand, query.getBrand())
-                .eq(StrUtil.isNotBlank(query.getCategory()), Item::getCategory, query.getCategory())
-                .eq(Item::getStatus, 1)
-                .between(query.getMaxPrice() != null, Item::getPrice, query.getMinPrice(), query.getMaxPrice())
-                .page(query.toMpPage("update_time", false));
-        // 封装并返回
+//        // 分页查询
+//        Page<Item> result = searchService.lambdaQuery()
+//                .like(StrUtil.isNotBlank(query.getKey()), Item::getName, query.getKey())
+//                .eq(StrUtil.isNotBlank(query.getBrand()), Item::getBrand, query.getBrand())
+//                .eq(StrUtil.isNotBlank(query.getCategory()), Item::getCategory, query.getCategory())
+//                .eq(Item::getStatus, 1)
+//                .between(query.getMaxPrice() != null, Item::getPrice, query.getMinPrice(), query.getMaxPrice())
+//                .page(query.toMpPage("update_time", false));
+//        // 封装并返回
+
+        // 通过es 查询
+        Page<ItemDoc> result = searchService.esSearch(query);
         return PageDTO.of(result, ItemDTO.class);
+    }
+
+    @PostMapping("/filters")
+    @ApiOperation("获取商品筛选条件")
+    public Map<String, List<String>> filters(@RequestBody ItemPageQuery query) {
+        return searchService.esAggregate(query);
     }
 }
--- search-service/src/main/java/com/hmall/search/service/impl/SearchServiceImpl.java
+++ search-service/src/main/java/com/hmall/search/service/impl/SearchServiceImpl.java
@@ -1,11 +1,152 @@
 package com.hmall.search.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmall.search.domain.po.Item;
+import com.hmall.search.domain.po.ItemDoc;
+import com.hmall.search.domain.query.ItemPageQuery;
 import com.hmall.search.mapper.SearchMapper;
 import com.hmall.search.service.ISearchService;
+import io.netty.util.internal.StringUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.*;
+
+@Slf4j
 @Service
 public class SearchServiceImpl extends ServiceImpl<SearchMapper, Item> implements ISearchService {
+    private final RestHighLevelClient client = new RestHighLevelClient(
+            RestClient.builder(HttpHost.create("http://192.168.100.128:9200"))
+    );
+    @Override
+    public Page<ItemDoc> esSearch(ItemPageQuery query) {
+        SearchRequest request = new SearchRequest("items");
+        BoolQueryBuilder bool = QueryBuilders.boolQuery();
+
+        if (StrUtil.isNotBlank(query.getKey())) {
+            bool.must(QueryBuilders.matchQuery("name", query.getKey()));
+        }
+        if (StrUtil.isNotBlank(query.getCategory())) {
+            bool.filter(QueryBuilders.termQuery("category", query.getCategory()));
+        }
+        if (StrUtil.isNotBlank(query.getBrand())) {
+            bool.filter(QueryBuilders.termQuery("brand", query.getBrand()));
+        }
+        if (query.getMinPrice() != null) {
+            bool.filter(QueryBuilders.rangeQuery("price").gte(query.getMinPrice()));
+        }
+        if (query.getMaxPrice() != null) {
+            bool.filter(QueryBuilders.rangeQuery("price").lte(query.getMaxPrice()));
+        }
+        if (StrUtil.isNotBlank(query.getSortBy())) {
+            request.source().sort(query.getSortBy(), query.getIsAsc() ? SortOrder.ASC : SortOrder.DESC);
+        }
+        if (query.getPageNo() != null && query.getPageSize() != null) {
+            request.source().from((query.getPageNo() - 1) * query.getPageSize()).size(query.getPageSize());
+        }
+
+        request.source().query(bool);
+
+        SearchResponse search = null;
+        try {
+            search = client.search(request, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (search == null)  {
+            log.error("查询结果为空！");
+            return null;
+        }
+
+        Page<ItemDoc> ans = new Page<>();
+        ans.setRecords(new java.util.ArrayList<>());
+        SearchHits hits = search.getHits();
+        if (hits.getTotalHits() != null) {
+            ans.setTotal(hits.getTotalHits().value);
+        }
+        SearchHit[] hits1 = hits.getHits();
+        for (SearchHit hit : hits1) {
+            ItemDoc bean = JSONUtil.toBean(hit.getSourceAsString(), ItemDoc.class);
+            ans.getRecords().add(bean);
+        }
+        return ans;
+    }
+
+    @Override
+    public Map<String, List<String>> esAggregate(ItemPageQuery query) {
+        Map<String, List<String>> map = new HashMap<>();
+        if (StrUtil.isBlank(query.getKey())) {
+            map.put("category", new ArrayList<>(Arrays.asList("手机", "拉杆箱", "休闲鞋", "硬盘", "真皮包")));
+            map.put("brand", new ArrayList<>(Arrays.asList("希捷", "小米", "华为", "OPPO", "尤妮佳", "OPPO", "Apple", "锤子")));
+            return map;
+        }
+        map.put("category", new ArrayList<>());
+        map.put("brand", new ArrayList<>());
+
+        SearchRequest request = new SearchRequest("items");
+        BoolQueryBuilder bool = QueryBuilders.boolQuery();
+        if (StrUtil.isNotBlank(query.getKey())) {
+            bool.filter(QueryBuilders.matchQuery("name", query.getKey()));
+        }
+        if (StrUtil.isNotBlank(query.getCategory())) {
+            bool.filter(QueryBuilders.termQuery("category", query.getCategory()));
+        }
+        if (StrUtil.isNotBlank(query.getBrand())) {
+            bool.filter(QueryBuilders.termQuery("brand", query.getBrand()));
+        }
+        if (query.getMinPrice() != null) {
+            bool.filter(QueryBuilders.rangeQuery("price").gte(query.getMinPrice()));
+        }
+        if (query.getMaxPrice() != null) {
+            bool.filter(QueryBuilders.rangeQuery("price").lte(query.getMaxPrice()));
+        }
+        request.source().size(0);
+        request.source().query(bool);
+        request.source().aggregation(AggregationBuilders.terms("category_agg").field("category").size(3));
+        request.source().aggregation(AggregationBuilders.terms("brand_agg").field("brand").size(3));
+
+        SearchResponse search = null;
+
+        try {
+            search = client.search(request, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (search == null) {
+            log.error("查询结果为空！");
+            return null;
+        }
+        Aggregations aggregations = search.getAggregations();
+        Terms terms = aggregations.get("category_agg");
+        for (Terms.Bucket bucket : terms.getBuckets()) {
+            map.get("category").add(bucket.getKeyAsString());
+        }
+        terms = aggregations.get("brand_agg");
+        for (Terms.Bucket bucket : terms.getBuckets()) {
+            map.get("brand").add(bucket.getKeyAsString());
+        }
+        return map;
+    }
 }
